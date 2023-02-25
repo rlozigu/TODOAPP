e87daf27af5e0dd556c662b35baffdc5320a3ab3
rlozigu <rlozigu@gmail.com>
2023-02-25 14:15:48 +0900

    routing - app.get()

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const app = express();
 
 //서버 생성(포트, 함수)
 app.listen(8080, function(){
-    console.log('listening on 8080');
+    console .log('listening on 8080');
 });
 
+app.get('/pet', function(req, res){
+    res.send('펫 페이지입니다.');
+})
+
+app.get('/beauty', function(req, res){
+    res.send('뷰티 페이지입니다.');
+})
