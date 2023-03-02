e7150dcb22d16f4053014c9b6552d2169ad88769
rlozigu <rlozigu@gmail.com>
2023-03-02 14:23:29 +0900

    post - 폼 데이터 서버로 전송

--- server.js
+++ server.js
@@ -2,12 +2,18 @@
 const express = require('express');
 //객체 생성
 const app = express();
+app.use(express.urlencoded({extended: true}))
 
 //서버 생성(포트, 함수)
 app.listen(8080, function(){
     console .log('listening on 8080');
 });
 
+/*get(param1, param2)
+param2의 형태가 함수임
+이와 같은 함수 안의 함수 구조를 콜백 함수라고 부른다.
+-> 순차적으로 실행하고 싶을 때 사용
+*/
 app.get('/pet', function(req, res){
     res.send('펫 페이지입니다.');
 })
@@ -17,5 +23,13 @@ app.get('/beauty', function(req, res){
 })
 
 app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.get('/write', function(req, res){
     res.sendFile(__dirname + '/write.html');
+})
+
+app.post('/add', function(req, res){
+    res.send('전송완료');
 })
